71d9dd8fe0ae0c159d4ac2420263b55b4320cf93
NapoleonQ <n90880215@gmail.com>
2026-08-12 07:30:17 -0400

    Archivo 'end_sep.py' creado

---
+++ Pruebas/end_sep.py
@@ -0,0 +1,7 @@
+# End: Elimina saltos de Linea y anade cadenas de caracteres
+print("Esto es una", end=" ")
+print("prueba")
+
+
+# Sep: Da manejo a los espacios entre lasa cadenas de caracteres
+print("1","2","3","4","5", sep="|")
